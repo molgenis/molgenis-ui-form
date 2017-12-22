d09e28f1710158e0e28534ffcf02672dbecb81e8
Mark-de-Haan <markdehaan90@gmail.com>
2017-12-22 12:34:55 +0100

    Add validate mock to TextFieldComponent tests

--- test/unit/specs/field-types/TextFieldComponent.spec.js
+++ test/unit/specs/field-types/TextFieldComponent.spec.js
@@ -21,12 +21,21 @@ describe('TextFieldComponent unit tests', () => {
       }
     ]
   }
+
   const state = {
     $touched: false,
     $submitted: false,
     $invalid: false
   }
-  const propsData = {value: 'hallo', field: field, state: state}
+
+  const validate = () => {}
+
+  const propsData = {
+    value: 'hallo',
+    field: field,
+    state: state,
+    validate: validate
+  }
 
   const wrapper = mount(TextFieldComponent,
     {
@@ -38,20 +47,24 @@ describe('TextFieldComponent unit tests', () => {
   it('should load the component with "TextFieldComponent" as a name', () => {
     expect(TextFieldComponent.name).to.equal('TextFieldComponent')
   })
+
   it('renders correctly with minimal props', () => {
     expect(wrapper.contains('input')).to.equal(true)
     expect(wrapper.contains('label')).to.equal(true)
     expect(wrapper.contains('small')).to.equal(true)
   })
+
   it('should load default data with the help of props', () => {
     expect(wrapper.vm.localValue).to.equal('hallo')
   })
+
   it('should render the label correctly', () => {
     expect(wrapper.contains('label')).to.equal(true)
     const label = wrapper.find('label')
     expect(label.text()).to.equal('Test Field')
     expect(label.element.htmlFor).to.equal('test-field')
   })
+
   it('should render the description correctly', () => {
     expect(wrapper.contains('small')).to.equal(true)
     const description = wrapper.find('small')
@@ -59,6 +72,7 @@ describe('TextFieldComponent unit tests', () => {
     expect(description.element.id).to.equal('test-field-description')
     expect(description.element.className).to.equal('form-text text-muted')
   })
+
   it('should render the input correctly', () => {
     expect(wrapper.contains('input')).to.equal(true)
     const input = wrapper.find('input').element
@@ -68,6 +82,7 @@ describe('TextFieldComponent unit tests', () => {
     expect(input.required).to.equal(true)
     expect(input.className).to.equal('form-control form-control-lg')
   })
+
   it('should emit an updated value on change', () => {
     wrapper.setData({localValue: 'test'})
     expect(wrapper.emitted().input[0]).to.deep.equal(['test'])
@@ -75,6 +90,7 @@ describe('TextFieldComponent unit tests', () => {
     wrapper.setData({localValue: 'test another'})
     expect(wrapper.emitted().input[1]).to.deep.equal(['test another'])
   })
+
   it('should receive the "is-invalid" class if not valid', () => {
     wrapper.setData({
       state: {
@@ -84,6 +100,7 @@ describe('TextFieldComponent unit tests', () => {
     })
     expect(wrapper.find('input').classes()).to.deep.equal(['form-control', 'form-control-lg', 'is-invalid'])
   })
+
   it('should show a field message if input is invalid', () => {
     wrapper.setData({
       state: {
