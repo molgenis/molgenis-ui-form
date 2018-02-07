2fce035d82403f281908a83040b41c1eda2b024b
Mark-de-Haan <markdehaan90@gmail.com>
2018-02-07 15:09:43 +0100

    Add flow typing

--- src/util/helpers/isCompoundVisible.js
+++ src/util/helpers/isCompoundVisible.js
@@ -1,11 +1,15 @@
+// @flow
+
+import type {FormField} from '../../flow.types'
+
 /**
  * Checks whether a compound should be shown based on the visibility of its children
  *
  * @param field the compound field
  * @param data the entire data of the form
  */
-export default function isCompoundVisible (field, data) {
-  return field.children.some(child => {
+export default function isCompoundVisible (field: FormField, data: ?Object): boolean {
+  return field.type === 'field-group' && field.children !== undefined && field.children.some(child => {
     if (child.type === 'field-group') {
       return isCompoundVisible(child, data)
     }
