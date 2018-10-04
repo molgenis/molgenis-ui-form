e9a76f9b3bb82fe2de5168ef5cfe49ffa5534597
connorstroomberg <ConnorStroomberg@users.noreply.github.com>
2018-10-04 17:28:21 +0200

    Add e2e test triggering form update on setting file

--- test/e2e/specs/file-field-test.js
+++ test/e2e/specs/file-field-test.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-expressions */
+const path = require('path')
 
 module.exports = {
   tags: ['file'], // run this suite with 'yarn e2e --tag file'
@@ -13,6 +14,16 @@ module.exports = {
     browser.expect.element('#alt-file-example').to.be.present
     // bootstrap uses label to fake file input value
     browser.expect.element('#file-example-fs > div > div > div.custom-file > label').text.to.contain('test-file-name.txt')
+    browser.end()
+  },
+
+  'File field allow selecting a file': function (browser) {
+    browser.options.desiredCapabilities.name = 'File field allow selecting a file'
+    browser.expect.element('#alt-file-example').to.be.present
+    // bootstrap uses label to fake file input value
+    browser.setValue('#alt-file-example', path.resolve(path.join(__dirname, 'file-field-test.js')))
+    // test form state as file-example itself is hidden
+    browser.expect.element('#file-example').to.have.attribute('class').which.contains('vf-form-dirty vf-form-valid vf-form-touched')
     browser.end()
   }
 
