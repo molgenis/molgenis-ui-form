de582181fa4c99f1b32de58dcb31a6993ba41e4e
connor <c.stroomberg@gmail.com>
2018-10-12 14:05:33 +0200

    test safari toDate f***up workaround

--- test/e2e/specs/date-time-field-test.js
+++ test/e2e/specs/date-time-field-test.js
@@ -12,12 +12,21 @@ module.exports = {
   'Datetime field should be valid with valid datetime string': function (browser) {
     browser.options.desiredCapabilities.name = 'Datetime field only valid for datetime string'
     browser.expect.element('#datetime-example-field').to.be.present
-    browser.getValue('#datetime-example-field ', function (result) {
-      var format = 'Y-MM-DD\\Thh:mm:ssZ'
-      var utcResult = moment(result.value, format, true).utc().toDate().getTime()
-      var utcExpected = moment('1985-08-12T08:12:13+02:00', format, true).utc().toDate().getTime()
-      this.assert.equal(utcResult, utcExpected)
-    })
+    if (browser.options.desiredCapabilities.browserName !== 'safari') {
+      browser.getValue('#datetime-example-field ', function (result) {
+        var format = 'Y-MM-DD\\Thh:mm:ssZ'
+        var utcResult = moment(result.value, format, true).utc().toDate().getTime()
+        var utcExpected = moment('1985-08-12T08:12:13+02:00', format, true).utc().toDate().getTime()
+        this.assert.equal(utcResult, utcExpected)
+      })
+    } else {
+      browser.getValue('#datetime-example-field ', function (result) {
+        var format = 'Y-MM-DD\\Thh:mm:ssZ'
+        var utcResult = moment(result.value, format, true).utc().unix()
+        var utcExpected = moment('1985-08-12T08:12:13+02:00', format, true).utc().unix()
+        this.assert.equal(utcResult, utcExpected)
+      })
+    }
     browser.expect.element('#datetime-example-field').to.have.attribute('class').which.contains('vf-valid')
     browser.end()
   }
