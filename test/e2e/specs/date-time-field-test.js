7c5dce27ea00583a1d1c0586a99e5097455586af
connor <c.stroomberg@gmail.com>
2018-10-12 16:02:21 +0200

    teat this please

--- test/e2e/specs/date-time-field-test.js
+++ test/e2e/specs/date-time-field-test.js
@@ -14,19 +14,19 @@ module.exports = {
     browser.expect.element('#datetime-example-field').to.be.present
     if (browser.options.desiredCapabilities.browserName !== 'safari') {
       browser.getValue('#datetime-example-field ', function (result) {
-        var format = 'Y-MM-DD\\Thh:mm:ssZ'
+        var format = moment.ISO_8601
         var utcResult = moment(result.value, format, true).utc().toDate().getTime()
         var utcExpected = moment('1985-08-12T08:12:13+02:00', format, true).utc().toDate().getTime()
         this.assert.equal(utcResult, utcExpected)
       })
     } else {
       browser.getValue('#datetime-example-field ', function (result) {
-        var format = 'Y-MM-DD\\Thh:mm:ssZ'
+        var format = moment.ISO_8601
         var utcResult = moment(result.value, format, true).utc()
         var utcExpected = moment('1985-08-12T08:12:13+02:00', format, true).utc()
         this.assert.equal(utcResult.year(), utcExpected.year())
-        this.assert.equal(utcResult.month(), utcExpected.month())
-        this.assert.equal(utcResult.dayOfYear(), utcExpected.dayOfYear())
+        this.assert.equal(utcResult.month(), utcExpected.month()) // month is zero indexed
+        this.assert.equal(utcResult.date(), utcExpected.date())
         this.assert.equal(utcResult.hour(), utcExpected.hour())
         this.assert.equal(utcResult.minute(), utcExpected.minute())
         this.assert.equal(utcResult.second(), utcExpected.second())
