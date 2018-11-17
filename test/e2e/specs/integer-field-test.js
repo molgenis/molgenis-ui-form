4e23f5ca5e173b98ec622fc967fb4adbb321a6bd
connor <c.stroomberg@gmail.com>
2018-11-17 14:20:41 +0100

    use browser locale to determine decimal separator

--- test/e2e/specs/integer-field-test.js
+++ test/e2e/specs/integer-field-test.js
@@ -1,7 +1,19 @@
 /* eslint-disable no-unused-expressions */
+const commaLocales = ['GB', 'US', 'en', 'en-GB', 'en-US', 'en-us']
+let separator = '.'
 
 module.exports = {
   tags: ['integer'], // run this suite with 'yarn e2e --tag integer'
+  before: function (browser) {
+    browser.execute(function () { return (navigator.language || navigator.languages[0]) },
+      [],
+      function (result) {
+        console.log('browser locale: ' + result.value)
+        separator = commaLocales.includes(result.value) ? ',' : '.'
+      }
+    )
+  },
+
   beforeEach: function (browser) {
     // Wait for form to be loaded
     browser.url(browser.globals.devServerURL)
@@ -20,13 +32,15 @@ module.exports = {
 
   'Integer field should be invalid with decimal value': function (browser) {
     browser.options.desiredCapabilities.name = 'Integer field not valid for decimal value'
+    console.log('using separator: ' + separator)
     browser.expect.element('#integer-example').to.be.present
 
     browser.click('#integer-example') // https://github.com/nightwatchjs/nightwatch/issues/504
-    browser.clearValue('#integer-example')
-    browser.setValue('#integer-example', '0.25')
     browser.pause(1000)
-    browser.setValue('#integer-example', '0,25')
+    // Send backspace as workaround for nightwatch clearValue issues
+    browser.setValue('#integer-example', '\u0008')
+    browser.pause(1000)
+    browser.setValue('#integer-example', '0' + separator + '5')
 
     browser.keys(browser.Keys.TAB)
     browser.click('h5.card-header.text-center') // click outside of input to trigger validation
