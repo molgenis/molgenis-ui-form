7d57e9584bb3812bc478adc11db84e7d2978cf1c
connor <c.stroomberg@gmail.com>
2018-11-17 21:48:25 +0100

    And now for something completely different

--- test/e2e/specs/integer-field-test.js
+++ test/e2e/specs/integer-field-test.js
@@ -1,18 +1,7 @@
 /* eslint-disable no-unused-expressions */
-const commaLocales = ['GB', 'US', 'en', 'en-GB', 'en-US', 'en-us']
-let separator = '.'
 
 module.exports = {
   tags: ['integer'], // run this suite with 'yarn e2e --tag integer'
-  before: function (browser) {
-    browser.execute(function () { return (navigator.language || navigator.languages[0]) },
-      [],
-      function (result) {
-        console.log('browser locale: ' + result.value)
-        separator = commaLocales.includes(result.value) ? ',' : '.'
-      }
-    )
-  },
 
   beforeEach: function (browser) {
     // Wait for form to be loaded
@@ -32,28 +21,29 @@ module.exports = {
 
   'Integer field should be invalid with decimal value': function (browser) {
     browser.options.desiredCapabilities.name = 'Integer field not valid for decimal value'
-    console.log('using separator: ' + separator)
     browser.expect.element('#integer-example').to.be.present
-
-    browser.click('#integer-example') // https://github.com/nightwatchjs/nightwatch/issues/504
-    browser.pause(1000)
-    // Send backspace as workaround for nightwatch clearValue issues
+    // clear the field
     browser.setValue('#integer-example', '\u0008')
     browser.pause(1000)
-    browser.setValue('#integer-example', '0')
-    browser.pause(1000)
-    browser.setValue('#integer-example', separator)
-    browser.pause(1000)
-    browser.setValue('#integer-example', '5')
 
-
-    browser.keys(browser.Keys.TAB)
-    browser.click('h5.card-header.text-center') // click outside of input to trigger validation
+    // test separator
+    browser.setValue('#integer-example', '1.2')
     browser.pause(1000)
-    browser.expect.element('#integer-example').to.have.attribute('class').which.contains('vf-invalid-integer')
-    browser.expect.element('.invalid-message').to.be.present
-    browser.expect.element('.invalid-message').text.to.be.equal('Not a valid integer value')
-    browser.end()
+
+    browser.getValue('#integer-example', function (result) {
+      if (result.value !== '1.2') {
+        // try with other separator
+        browser.setValue('#integer-example', '\u0008')
+        browser.setValue('#integer-example', '1,2')
+        browser.pause(1000)
+      }
+
+      browser.pause(1000)
+      browser.expect.element('#integer-example').to.have.attribute('class').which.contains('vf-invalid-integer')
+      browser.expect.element('.invalid-message').to.be.present
+      browser.expect.element('.invalid-message').text.to.be.equal('Not a valid integer value')
+      browser.end()
+    })
   },
 
   'Clearing the value should place "null" in the model': function (browser) {
