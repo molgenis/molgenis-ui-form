3761430b3ee2dd3edbfb350db51d2596cac0c805
connor <c.stroomberg@gmail.com>
2018-11-18 08:35:01 +0100

    move empty test to avoid conflict on ff browser

--- test/e2e/specs/integer-field-test.js
+++ test/e2e/specs/integer-field-test.js
@@ -47,23 +47,5 @@ module.exports = {
       browser.setValue('#integer-example', '\u0008')
       browser.end()
     })
-  },
-
-  'Clearing the value should place "null" in the model': function (browser) {
-    browser.options.desiredCapabilities.name = 'Clearing the value should place "null" in the model'
-    browser.expect.element('#integer-example').to.be.present
-
-    browser.click('#integer-example') // https://github.com/nightwatchjs/nightwatch/issues/504
-    browser.pause(1000)
-    // Send backspace as workaround for nightwatch clearValue issues
-    browser.setValue('#integer-example', '\u0008')
-    browser.pause(1000)
-
-    browser.expect.element('#integer-example').to.have.attribute('class').which.contains('vf-valid')
-    browser.getText('.field-data-json', function (result) {
-      this.assert.equal(JSON.parse(result.value)['integer-example'], null)
-    })
-
-    browser.end()
   }
 }
