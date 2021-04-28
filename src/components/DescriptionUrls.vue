a1c9a5695478d0e687fd7d317df4a3c25ea6aa30
Max Postema <m.e.postema@rug.nl>
2021-04-28 09:44:44 +0200

    fix: force urls starting with www. to be external urls

--- src/components/DescriptionUrls.vue
+++ src/components/DescriptionUrls.vue
@@ -2,7 +2,7 @@
   <span>
     <span v-for="(item, index) in description" :key="`desc-${index}`">
       <span>{{ item.text }}</span>
-      <a v-if="item.url" :key="`url-${index}`" :href="item.url" target="_blank">{{item.url}}</a>
+      <a v-if="item.url" :key="`url-${index}`" :href="ExternalURL(item.url)" target="_blank">{{item.url}}</a>
     </span>
   </span>
 </template>
@@ -14,6 +14,14 @@ export default {
     description: {
       type: Array,
       required: true
+    }
+  },
+  methods: {
+    ExternalURL (url) {
+      if (!url.match(/^[a-zA-Z]+:\/\//)) {
+        url = 'https://' + url
+      }
+      return url
     }
   }
 }
