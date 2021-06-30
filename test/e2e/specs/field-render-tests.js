7ad184fb267624261560bca548be4c3f2ddc110d
Fleur Kelpin <f.kelpin@umcg.nl>
2021-06-30 10:57:11 +0200

    test: click calendar button instead

--- test/e2e/specs/field-render-tests.js
+++ test/e2e/specs/field-render-tests.js
@@ -124,7 +124,7 @@ module.exports = {
 
     browser.expect.element('.flatpickr-calendar.open .today').to.be.present
     browser.click('.flatpickr-calendar.open .today')
-    browser.click('#form-demo') // click outside calender (work around for IE11 issue: https://github.com/chmln/flatpickr/issues/900)
+    browser.click('#date .button') // click calendar button
     browser.expect.element('.flatpickr-calendar.open').to.be.not.present
 
     const today = moment().toJSON().slice(0, 10)
