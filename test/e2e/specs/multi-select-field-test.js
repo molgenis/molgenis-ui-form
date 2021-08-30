35d6f2d1329135cf1e6d1fd0d3bc0592dc5b6f6e
Fleur Kelpin <f.kelpin@umcg.nl>
2021-08-30 22:47:18 +0200

    test: fix multi select e2e tests

--- test/e2e/specs/multi-select-field-test.js
+++ test/e2e/specs/multi-select-field-test.js
@@ -12,10 +12,10 @@ module.exports = {
     browser.options.desiredCapabilities.name = 'Select multi select value'
     browser.expect.element('#multi-select-form-example').to.be.present
     browser.expect.element('#multi-select-form-example.vf-form-untouched.vf-form-pristine').to.be.present
-    browser.expect.element('.v-select.vf-untouched.vf-pristine').to.be.present
+    browser.expect.element('.multiselect.vf-untouched.vf-pristine').to.be.present
     browser.click('#multi-select-form-example')
-    browser.setValue('input[type=search]', ['o'])
-    browser.setValue('input[type=search]', [browser.Keys.ENTER])
+    browser.setValue('input[type=text]', ['o'])
+    browser.setValue('input[type=text]', [browser.Keys.ENTER])
     browser.expect.element('#multi-select-form-example.vf-form-dirty.vf-form-valid.vf-form-touched').to.be.present
     browser.end()
   },
@@ -24,7 +24,7 @@ module.exports = {
     browser.options.desiredCapabilities.name = 'Empty search param still yields options'
     browser.expect.element('#multi-select-form-example').to.be.present
     browser.click('#multi-select-form-example')
-    browser.assert.elementCount('ul.dropdown-menu > li', 5)
+    browser.assert.elementCount('ul.multiselect__content > li.multiselect__element', 5)
     browser.end()
   }
 }
