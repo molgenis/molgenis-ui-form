bd035aacd0a3aa773755ab5ff2f1dba4bb2faaf7
Postema Max <max@elynor.com>
2022-01-28 14:36:00 +0100

    feat: ability to add brakes in descriptions

--- src/components/DescriptionUrls.vue
+++ src/components/DescriptionUrls.vue
@@ -1,14 +1,17 @@
 <template>
   <span>
-    <span v-for="(item, index) in descriptionSplit" :key="`desc-${index}`">
-      <span>{{ item.text }}</span>
+    <span v-for="(item, urlIndex) in textURLSplit(description)" :key="`desc-${urlIndex}`">
+      <span v-for="(text, breakIndex) in textBreakSplit(item.text)" :key="`desc-text-${breakIndex}`">
+        <br v-if="breakIndex>0">
+        {{ text }}
+      </span>
       <a
         v-if="item.url"
-        :key="`url-${index}`"
+        :key="`url-${urlIndex}`"
         :href="ExternalURL(item.url)"
-        target="_blank"
-        >{{ item.url }}</a
-      >
+        target="_blank">
+          {{ item.second }}
+      </a>
     </span>
   </span>
 </template>
@@ -22,17 +25,13 @@ export default {
       required: true
     }
   },
-  computed: {
-    descriptionSplit () {
-      return this.textURLSplit(this.description)
-    }
-  },
   methods: {
-    // Returns an array of structure [{text, url}, {text, url}, ...]
-    // Note: split may be [{text}] if no url is found
     textURLSplit (text) {
       const splittedText = text.split(/(?:[^\S]|^)((?:(?:https?:\/\/)|(?:www\.))(?:\S+))/gi)
       return getTextAndUrls(splittedText)
+    },
+    textBreakSplit (text) {
+      return text.split('{br}')
     },
     ExternalURL (url) {
       if (!url.match(/^http/gmi)) {
